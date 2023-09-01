fbb506adef287bf2e057484958586e0738dad881
shriiiar <fahimshariar511@gmail.com>
2023-09-02 05:24:44 +0600

    config updates

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 // export const SERVER_URL = `https://course-management-back-seven.vercel.app/api`;
 // export const SERVER_URL = `http://localhost:5000/api`;
 // export const SERVER_URL_FOR_SOCKET = `http://localhost:5000`;
-export const SERVER_URL = `https://course-management-server.onrender.com/api`;
-export const SERVER_URL_FOR_SOCKET = `https://course-management-server.onrender.com`;
+export const SERVER_URL = `https://coursera-back.onrender.com/api`;
+export const SERVER_URL_FOR_SOCKET = `https://coursera-back.onrender.com`;
